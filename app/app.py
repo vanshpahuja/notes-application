9b444a02b856f687b4da77559bced3007a8427d3
vanshpahuja <vanshpahuja24@gmail.com>
2026-08-08 12:50:08 +0530

    chore: changes made, added print statement

--- app/app.py
+++ app/app.py
@@ -136,4 +136,4 @@ def root():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", "8080")))
 
-print("Checking if changes are affecting")
+print("Checking if changes are affecting IN notes-gitops")
